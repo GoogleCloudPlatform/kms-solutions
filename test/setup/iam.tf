c1b749c2d691ad3a214c84364bea58c0f50cc2a2
Leonardo Henrique Romanini <leonardo.romanini@ciandt.com>
2024-04-17 16:30:53 -0300

    Add service identity again

--- test/setup/iam.tf
+++ test/setup/iam.tf
@@ -42,3 +42,10 @@ resource "google_project_iam_member" "int_test" {
 resource "google_service_account_key" "int_test" {
   service_account_id = google_service_account.int_test.id
 }
+
+resource "google_project_service_identity" "cb_sa" {
+  provider = google-beta
+
+  project = module.project_ci_kms.project_id
+  service = "cloudbuild.googleapis.com"
+}
