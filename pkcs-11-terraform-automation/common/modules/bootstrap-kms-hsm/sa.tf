7e88d8d7e8b8755ed2796780e7f2a9ae4b95b02f
Leonardo Henrique Romanini <leonardo.romanini@ciandt.com>
2024-04-29 18:06:17 -0300

    Add imporsonate 2

--- pkcs-11-terraform-automation/common/modules/bootstrap-kms-hsm/sa.tf
+++ pkcs-11-terraform-automation/common/modules/bootstrap-kms-hsm/sa.tf
@@ -81,11 +81,11 @@ resource "google_service_account_iam_member" "self_impersonation" {
   member             = "serviceAccount:${local.custom_sa_email}"
 }
 
-# resource "google_service_account_iam_member" "cb_service_agent_impersonate_2" {
-#   service_account_id = local.custom_sa_name
-#   role               = "roles/iam.serviceAccountTokenCreator"
-#   member             = "serviceAccount:service-${data.google_project.cloudbuild_project.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"
-# }
+resource "google_service_account_iam_member" "cb_service_agent_impersonate_2" {
+  service_account_id = local.custom_sa_name
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "serviceAccount:service-${data.google_project.cloudbuild_project.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"
+}
 
 resource "google_project_iam_member" "sa_service_account_user" {
   project = var.project_id
