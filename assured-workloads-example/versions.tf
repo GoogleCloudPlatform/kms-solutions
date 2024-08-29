9ceb3db27adf6e122bca90301618541513a15c18
Kieras <akieras@ciandt.com>
2024-08-29 11:28:35 -0300

    Updating the version to the resolved number.

--- assured-workloads-example/versions.tf
+++ assured-workloads-example/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.77"
+      version = ">= 5.43"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.77"
+      version = ">= 5.43"
     }
     random = {
       source  = "hashicorp/random"
